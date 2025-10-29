57d179abf75859f61db4eb1483b24c1030ebd8ec
Alina Visan <visan.alina2005@gmail.com>
2025-10-29 23:08:12 +0200

    // am creat domeniul cu numele Veterinar, cele 3 obiecte: Vaccin, VizitaMedicala si EchipamentMedical. Le am dat momentan doar atribute normale.

--- Activitate_POO_2025/ActivitatePOO.cpp
+++ Activitate_POO_2025/ActivitatePOO.cpp
@@ -1,3 +1,39 @@
-//TEST
-//test
-//testtt
+#include<iostream> // librarie care contine diferite functii
+#include<fstream> //partea de intrare
+#include<string>
+
+using namespace std; // folosim acest spatiu standard ca la cout si cin sa nu mai punem operatorul de rezolutie std::
+// int numele clasei::atribut=0; // initializare atribut static , variabila globala
+
+class Veterinar//domeniu, generalizare, upper camel case
+{
+	class Vaccin //obiectul1
+	{
+		int idVaccin;
+		char* InstructiuniAplicare;
+		string numeVaccin;
+		float greutate;
+	};
+
+	class VizitaMedicala //obiectul2
+	{
+		int idPacient;
+		char* numeMedic;
+		string DataVizitei;
+		float pulAnimalBPM;
+
+	};
+
+	class EchipamentMedical// obiectul 3 
+	{
+		int idInventar;
+		char* sectieUtilizare;
+		string numeInstrument;
+		float temperatura;
+	};
+
+ 
+int main()
+	{
+
+	}
