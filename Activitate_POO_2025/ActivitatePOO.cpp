fecdf180a418eedb47d5c860773b8b434ef53fa5
Alina Visan <visan.alina2005@gmail.com>
2025-11-02 16:41:33 +0200

    // am stabilit atributele constante si am facut constructorii cu parametrii de initializare. nu am stabilit inca atributele statistice

--- Activitate_POO_2025/ActivitatePOO.cpp
+++ Activitate_POO_2025/ActivitatePOO.cpp
@@ -3,33 +3,66 @@
 #include<string>
 
 using namespace std; // folosim acest spatiu standard ca la cout si cin sa nu mai punem operatorul de rezolutie std::
-// int numele clasei::atribut=0; // initializare atribut static , variabila globala
+// int numele clasei::atribut=0; // initializare atribut static, variabila globala
 
 class Veterinar//domeniu, generalizare, upper camel case
 {
 	class Vaccin //obiectul1
 	{
-		int idVaccin;
-		char* InstructiuniAplicare;
-		string numeVaccin;
-		float greutate;
-	};
+	public:
+		const int idVaccin;
+		int nrDoze;
+		char* numeVaccin;
+		string instructiuniAplicare;
+		float geutateVaccin;
 
+		Vaccin(int nrDoze, string instructiuniAplicare,const char* numeVaccin, float geutateVaccin) // constructor cu parametrii, initializare
+		{
+			this->nrDoze= nrDoze;
+			this->instructiuniAplicare = instructiuniAplicare;
+			this->geutateVaccin= geutateVaccin;
+			this->numeVaccin = new char[strlen(numeVaccin) + 1];
+			strcpy_s(this->numeVaccin, strlen(numeVaccin) + 1, numeVaccin);
+		}
+	};
 	class VizitaMedicala //obiectul2
 	{
-		int idPacient;
-		char* numeMedic;
-		string DataVizitei;
-		float pulAnimalBPM;
+	public:
+		const int idPacient;
+		int nrVaccinuri;
+		char* numePacient;
+		string numeMedic;
+		//int* nrConsultatii;
+		float greutateAnimal;
 
+
+		Vizita medicala(int nrVaccinuri, const char* numePacient, string numeMedic, float greutateAnimal) //constructor cu parametrii, initializare
+		{
+			this->nrVaccinuri = nrVaccinuri;
+			this->numeMedic = numeMedic;
+			this->greutateAnimal = greutateAnimal;
+			this->numePacient = new char[strlen(numePacient) +1 ];
+			strcpy_s(this->numePacient, strlen(numePacient) + 1, numePacient);
+		}
 	};
 
 	class EchipamentMedical// obiectul 3 
 	{
-		int idInventar;
-		char* sectieUtilizare;
-		string numeInstrument;
+	public:
+		const int idInventar;
+		int garantieAni;
+		char* numeInstrument;
+		string dataExpirare;
 		float temperatura;
+
+		EchipamentMedical(int garantieAni, const char* numeInstrument, string dataExpirare, float temperatura) //constructori cu paramterii, initializare
+		{
+			this->garantieAni = garantieAni;
+			this-> dataExpirare= dataExpirare ;
+			this->temperatura = temperatura;
+			this->numeInstrument = new char[strlen(numeInstrument) + 1];
+			strcpy_s(this->numeInstrument, strlen(numeInstrument) + 1, numeInstrument);
+		}
 	};
 
  
