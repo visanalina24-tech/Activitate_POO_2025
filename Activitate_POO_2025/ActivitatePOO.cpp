672f429823b7ff2a1ac503390e3d8ed27ea3a84c
Alina Visan <visan.alina2005@gmail.com>
2025-11-10 19:39:40 +0200

    // am sters prima clasa ca sa le pot apela pe restul, o sa o adaug mai tarziu

--- Activitate_POO_2025/ActivitatePOO.cpp
+++ Activitate_POO_2025/ActivitatePOO.cpp
@@ -1,72 +1,100 @@
-#include<iostream> // librarie care contine diferite functii
+#include<iostream> //librarie care contine diferite functii
 #include<fstream> //partea de intrare
-#include<string>
+#include<string> 
 
-using namespace std; // folosim acest spatiu standard ca la cout si cin sa nu mai punem operatorul de rezolutie std::
+using namespace std; //folosim acest spatiu standard ca la cout si cin sa nu mai punem operatorul de rezolutie std::
 // int numele clasei::atribut=0; // initializare atribut static, variabila globala
 
-class Veterinar//domeniu, generalizare, upper camel case
-{
-	class Vaccin //obiectul1
-	{
-	public:
-		const int idVaccin;
-		int nrDoze;
-		char* numeVaccin;
-		string instructiuniAplicare;
-		float geutateVaccin;
 
-		Vaccin(int nrDoze, string instructiuniAplicare,const char* numeVaccin, float geutateVaccin) // constructor cu parametrii, initializare
+class Vaccin //obiectul1
+{
+private:
+	const int idVaccin;
+public:
+	int nrDoze;
+	char* numeVaccin;
+	string instructiuniAplicare;
+	float greutateVaccin;
+	static int nrDozeMaxime;
+
+	Vaccin() :idVaccin(++nrDozeMaxime)//constructor default
+	{
+		this->nrDoze = 2;
+		this->instructiuniAplicare = "1. Deschidere recipient medicament 2. Extragere cu seringa 3. Aplucarea spirtului pe membrul pacientului 4. Introducere ac de la vaccin in membrul pacientului";
+		this->greutateVaccin = 0.01;
+		this->numeVaccin = nullptr;
+		nrDozeMaxime++;
+	}
+
+	Vaccin(int nrDoze, string instructiuniAplicare, const char* numeVaccin, float greutateVaccin) //constructor cu parametrii, initializare
+	{
+		this->nrDoze = nrDoze;
+		this->instructiuniAplicare = instructiuniAplicare;
+		this->greutateVaccin = greutateVaccin;
+		if (numeVaccin != 0)
 		{
-			this->nrDoze= nrDoze;
-			this->instructiuniAplicare = instructiuniAplicare;
-			this->geutateVaccin= geutateVaccin;
 			this->numeVaccin = new char[strlen(numeVaccin) + 1];
 			strcpy_s(this->numeVaccin, strlen(numeVaccin) + 1, numeVaccin);
 		}
-	};
-	class VizitaMedicala //obiectul2
-	{
-	public:
-		const int idPacient;
-		int nrVaccinuri;
-		char* numePacient;
-		string numeMedic;
-		//int* nrConsultatii;
-		float greutateAnimal;
-
-
-		Vizita medicala(int nrVaccinuri, const char* numePacient, string numeMedic, float greutateAnimal) //constructor cu parametrii, initializare
+		else
 		{
-			this->nrVaccinuri = nrVaccinuri;
-			this->numeMedic = numeMedic;
-			this->greutateAnimal = greutateAnimal;
-			this->numePacient = new char[strlen(numePacient) +1 ];
-			strcpy_s(this->numePacient, strlen(numePacient) + 1, numePacient);
+			this->numeVaccin = nullptr;
 		}
-	};
-
-	class EchipamentMedical// obiectul 3 
-	{
-	public:
-		const int idInventar;
-		int garantieAni;
-		char* numeInstrument;
-		string dataExpirare;
-		float temperatura;
-
-		EchipamentMedical(int garantieAni, const char* numeInstrument, string dataExpirare, float temperatura) //constructori cu paramterii, initializare
-		{
-			this->garantieAni = garantieAni;
-			this-> dataExpirare= dataExpirare ;
-			this->temperatura = temperatura;
-			this->numeInstrument = new char[strlen(numeInstrument) + 1];
-			strcpy_s(this->numeInstrument, strlen(numeInstrument) + 1, numeInstrument);
-		}
-	};
-
- 
-int main()
-	{
-
 	}
+
+	// Destructor pentru eliberarea memoriei
+	~Vaccin()
+	{
+		delete[] numeVaccin;
+	}
+};
+
+
+/*/class VizitaMedicala //obiectul2
+{
+public:
+	static int
+	const int idPacient;
+	int nrVaccinuri;
+	char* numePacient;
+	string numeMedic;
+	//int* nrConsultatii;
+	float greutateAnimal;
+
+
+	VizitaMedicala(int nrVaccinuri, const char* numePacient, string numeMedic, float greutateAnimal) //constructor cu parametrii, initializare
+	{
+		this->nrVaccinuri = nrVaccinuri;
+		this->numeMedic = numeMedic;
+		this->greutateAnimal = greutateAnimal;
+		this->numePacient = new char[strlen(numePacient) + 1];
+		strcpy_s(this->numePacient, strlen(numePacient) + 1, numePacient);
+	}
+};
+
+class EchipamentMedical//obiectul3
+{
+public:
+	const int idInventar;
+	int garantieAni;
+	char* numeInstrument;
+	string dataExpirare;
+	float temperatura;
+
+	EchipamentMedical(int garantieAni, const char* numeInstrument, string dataExpirare, float temperatura) //constructori cu paramterii, initializare
+	{
+		this->garantieAni = garantieAni;
+		this->dataExpirare = dataExpirare;
+		this->temperatura = temperatura;
+		this->numeInstrument = new char[strlen(numeInstrument) + 1];
+		strcpy_s(this->numeInstrument, strlen(numeInstrument) + 1, numeInstrument);
+	}
+} */
+
+
+int main()
+{
+
+	Vaccin v1(2;
+
+}
